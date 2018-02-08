654ec6f36ceb9b5a5edad64d0567a3bd3567d165
Katja Krüger <krueger@life-online.de>
2018-02-08 09:51:52 +0100

    blocks

--- blocks/forum_aggregator/block_forum_aggregator.php
+++ blocks/forum_aggregator/block_forum_aggregator.php
@@ -13,10 +13,14 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-/*
- * @package    block
- * @subpackage forum_aggregator
- * @author     Tõnis Tartes <t6nis20@gmail.com>
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Main functions.
+ *
+ * @package    block_forum_aggregator
+ * @author     Tonis Tartes <t6nis20@gmail.com>
  * @copyright  1999 onwards Martin Dougiamas  {@link http://moodle.com}
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
@@ -53,7 +57,7 @@ class block_forum_aggregator extends block_base {
         
         global $DB, $CFG, $USER, $COURSE, $OUTPUT;
 
-        //Include needed libraries
+        // Include needed libraries.
         require_once($CFG->dirroot.'/mod/forum/lib.php'); 
         
         $this->content = new stdClass;
@@ -62,24 +66,24 @@ class block_forum_aggregator extends block_base {
         
         $text = '';
 
-        $modinfo = get_fast_modinfo($COURSE); //course mod data
+        $modinfo = get_fast_modinfo($COURSE);
 
         if (!empty($this->config->forum_id)) {
 
             foreach ($this->config->forum_id as $key => $value) {
 
-                    //if forum not been made available
+                    // If forum not been made available.
                     if ($value == 0) {
                         continue;
                     }
                     
                     if (empty($modinfo->instances['forum'][$key])) {
-                        //maybe someone deleted a forum? then skip that value..
+                        // Maybe someone deleted a forum? then skip that value.
                         continue;
                     }
                     
                     $max_posts = '';
-                    //if post in array get the maxpost value
+                    // If post in array get the maxpost value.
                     if (array_key_exists($key, $this->config->max_posts)) {
                         if ($this->config->max_posts[$key] > 0) {
                             $max_posts = $this->config->max_posts[$key];
@@ -95,25 +99,23 @@ class block_forum_aggregator extends block_base {
                     $strftimerecent = get_string('strftimerecent');
                     $strmore = get_string('more', 'forum');
                     
-                    //if visible
                     if ($cm->visible == 1) {
                         
                         if (! $forum = $DB->get_record("forum", array("id" => $key))) {
                             print_error('invalidforumid', 'forum');
                         }
-
-                        //show list                        
+                     
                         $text .= html_writer::start_tag('ul', array('class'=> 'unlist'));
-                        ///KK $text .= html_writer::tag('li', html_writer::link(new moodle_url('/mod/forum/view.php?id='.$cm->id), $cm->name), array('class' => 'forum_title'));
+                        $text .= html_writer::tag('li', html_writer::link(new moodle_url('/mod/forum/view.php?id='.$cm->id), $cm->name), array('class' => 'forum_title'));
                         
                         $allnames = get_all_user_name_fields(true, 'u');
                         $posts = $DB->get_records_sql('SELECT d.id, p.*, '.$allnames.', u.email, u.picture, u.imagealt
                                             FROM {forum_discussions} d
                                             LEFT JOIN {forum_posts} p ON p.discussion = d.id
                                             LEFT JOIN {user} u ON p.userid = u.id
-                                            WHERE d.forum = "'.$key.'"
-                                            ORDER BY p.created DESC LIMIT 0, '.$max_posts.'');
-                        //KK changed modified into created
+                                            WHERE d.forum = \''.$key.'\'
+                                            ORDER BY p.modified DESC ', null, 0, $max_posts);
+                        
                         if (!empty($posts)) {
                             
                             foreach ($posts as $post) {
@@ -126,23 +128,21 @@ class block_forum_aggregator extends block_base {
                                 }
                                 
                                 $post->message = format_string($post->message, true, $COURSE->id);                        
-                                $post->message = substr(strip_tags($post->message), 0, 150);
+                                $post->message = shorten_text($post->message, 180, true, '');
 
                                 $user = $DB->get_record('user', array('id'=>$post->userid), '*', MUST_EXIST);
                                 
                                 $text .= html_writer::start_tag('li', $post_style).
                                          html_writer::start_tag('div', array('class' => 'head')).
-                                         html_writer::tag('div', $OUTPUT->user_picture($user, array('size'=>40, 'class'=>'userpostpic')), array('class' => 'userpic'));
-                                $text .= '<div class="subject">';
-                                $text .=  html_writer::link(new moodle_url('/mod/forum/discuss.php?d='.$post->discussion.'#p'.$post->id), $post->subject);
-                                $text .= html_writer::end_tag('div').
-                                         html_writer::start_tag('div').
-                                         '<span class="name">'.fullname($post).' | </span><span class="date">'.userdate($post->created, $strftimerecent).'</span>'.
-                                         //html_writer::tag('div', fullname($post), array('class' => 'name')).
-                                         //html_writer::tag('div', userdate($post->created, $strftimerecent), array('class' => 'date')).
+                                         html_writer::tag('div', $post->subject, array('class' => 'subject')).
+                                         html_writer::tag('div', $OUTPUT->user_picture($user, array('size'=>21, 'class'=>'userpostpic')), array('class' => 'userpic')).
+                                         html_writer::tag('div', fullname($post), array('class' => 'name')).
+                                         html_writer::tag('div', get_string('posted', 'block_forum_aggregator').userdate($post->modified, $strftimerecent), array('class' => 'date')).
                                          html_writer::end_tag('div').
-                                         $post->message.' ... '.
+                                         html_writer::start_tag('div').
+                                         $post->message.' '.
                                          html_writer::link(new moodle_url('/mod/forum/discuss.php?d='.$post->discussion.'#p'.$post->id), $strmore.'...' , array('class' => 'postreadmore')).
+                                         html_writer::end_tag('div').
                                          html_writer::end_tag('li');
                             }
                         } else {
@@ -163,7 +163,7 @@ class block_forum_aggregator extends block_base {
 
         global $CFG;
 
-        // Default behavior: save all variables as $CFG properties
+        // Default behavior: save all variables as $CFG properties.
         if(get_config('forum_aggregator', 'Allow_HTML') == '1') {
             $data->title = strip_tags($data->title);
         }
@@ -171,5 +171,3 @@ class block_forum_aggregator extends block_base {
         return parent::instance_config_save($data, $nolongerused);
     }
 }
-
-?>
