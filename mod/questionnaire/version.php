9d57b9c2cacb3e75b0de7f5cc62b3fe31c2b8c34
Katja Krüger <krueger@life-online.de>
2019-11-05 11:22:38 +0100

    mod

--- mod/questionnaire/version.php
+++ mod/questionnaire/version.php
@@ -24,10 +24,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2018050109;  // The current module version (Date: YYYYMMDDXX)
-$plugin->requires = 2017042800; // Moodle version.
+$plugin->version  = 2018110101;  // The current module version (Date: YYYYMMDDXX)
+$plugin->requires = 2018120300; // Moodle version.
 
 $plugin->component = 'mod_questionnaire';
 
-$plugin->release  = '3.5.4 (Build - 2019032100)';
+$plugin->release  = '3.6.1 (Build - 2019052400)';
 $plugin->maturity  = MATURITY_STABLE;
